072ce6f32e5ffd9a2291f8635cf8cc9acd9546bd
JoaoYolli <yoaojoao@gmail.com>
2024-06-25 23:18:47 +0200

    Database migrated to postgress

--- database.js
+++ database.js
@@ -1,119 +1,75 @@
-const { OPEN_READWRITE } = require('sqlite3');
-const sqlite3 = require('sqlite3').verbose();
+require('dotenv').config();
+const { Pool } = require('pg');
 const fs = require('fs');
 
+// Configuración de la conexión a Vercel/Postgres
+// console.log(process.env.POSTGRES_URL)
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 async function initializeDB() {
+  var dir = './db';
 
-    var dir = './db';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
 
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
+  const client = await pool.connect();
 
-    let db = await openDatabase()
-
-    db.serialize(() => {
-        // Crea la tabla de usuarios
-        db.run(`CREATE TABLE IF NOT EXISTS users (
-            mail TEXT PRIMARY KEY,
-            akka CHAR(20) NOT NULL,
-            kills_as_wolf INTEGER NOT NULL,
-            wolfs_killed INTEGER NOT NULL,
-            UNIQUE (mail, akka)
-            )`, (err) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                console.log('Tabla creada exitosamente o ya existía.');
-            }
-        });
-        // Insertar un usuario
-        // db.run(`INSERT INTO users (mail, akka, kills_as_wolf, wolfs_killed) VALUES (?, ?, ? ,?)`, ['yoaojoao@gmail.com', 'desarrollador', 0, 0], function (err) {
-        //     if (err) {
-        //         return console.error(err.message);
-        //     } else {
-        //         console.log(`Un usuario ha sido insertado con el correo ${this.lastID}`);
-        //     }
-        // });
-    });
-
-    closeDB(db)
-}
-
-function openDatabase() {
-    return new Promise((resolve) => {
-        let database = new sqlite3.Database('./db/el-lobo.db', sqlite3.OPEN_CREATE || OPEN_READWRITE, (err) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                console.log('Connected to the wolf database.');
-            }
-        });
-        resolve(database)
-    })
-}
-
-function closeDB(db) {
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Close the database connection.');
-    });
+  try {
+    await client.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        mail TEXT PRIMARY KEY,
+        akka CHAR(20) NOT NULL,
+        kills_as_wolf INTEGER NOT NULL,
+        wolfs_killed INTEGER NOT NULL,
+        UNIQUE (mail, akka)
+      )
+    `);
+    console.log('Tabla creada exitosamente o ya existía.');
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    client.release();
+  }
 }
 
 async function getUserByMail(mail) {
+  const client = await pool.connect();
 
-    return new Promise(async (resolve) => {
-
-        let db = await openDatabase()
-
-        // Get usuario
-        db.get(`SELECT * 
-                            FROM users
-                            WHERE mail = '${mail}'
-                    `, (err, row) => {
-            if (err) {
-                console.error(err.message);
-            } else {
-                console.log("ROW", row)
-                resolve(row)
-            }
-        });
-
-        closeDB(db)
-
-    })
-
+  try {
+    const res = await client.query('SELECT * FROM users WHERE mail = $1', [mail]);
+    console.log("ROW", res.rows[0]);
+    return res.rows[0];
+  } catch (err) {
+    console.error(err.message);
+  } finally {
+    client.release();
+  }
 }
 
-function createUser(mail, akka) {
+async function createUser(mail, akka) {
+  const user = await getUserByMail(mail);
 
-    return new Promise(async (resolve) => {
+  if (user !== undefined) {
+    return "User already exists";
+  } else {
+    const client = await pool.connect();
 
-        let search = await getUserByMail(mail)
-
-        if(search !== undefined){
-            resolve("User alredy exists")
-        }else{
-            let db = await openDatabase()
-    
-            db.run(`INSERT INTO users (mail, akka, kills_as_wolf, wolfs_killed) VALUES (?, ?, ? ,?)`, [mail, akka, 0, 0], function (err) {
-                if (err) {
-                    return console.error(err.message);
-                } else {
-                    console.log(`Un usuario ha sido insertado con el correo ${this.lastID}`);
-                }
-            });
-    
-            closeDB(db)
-    
-            resolve("User created succesfully!")
-        }
-
-    })
-
+    try {
+      await client.query('INSERT INTO users (mail, akka, kills_as_wolf, wolfs_killed) VALUES ($1, $2, $3, $4)', [mail, akka, 0, 0]);
+      console.log(`Un usuario ha sido insertado con el correo ${mail}`);
+      return "User created successfully!";
+    } catch (err) {
+      console.error(err.message);
+    } finally {
+      client.release();
+    }
+  }
 }
 
-module.exports = { initializeDB, getUserByMail, createUser
-}
+module.exports = { initializeDB, getUserByMail, createUser };
--- serverExpress.js
+++ serverExpress.js
@@ -133,6 +133,7 @@ app.post("/join-game", (req, res) => {
 
 // Initialize DB and start the server
 db.initializeDB();
+db.createUser("yoaojoao@gmail.com", "Joao");
 const server = app.listen(port, /*host,*/ () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
